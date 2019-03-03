490e7f6efc0a400e564de376c923c5e2261c6c94
Hampus <hampus.lundblad@hotmail.se>
2019-03-03 20:00:59 +0100

    added some code from chap 3 and 4. No actual code for a2

--- a21.hs
+++ a21.hs
@@ -155,15 +155,12 @@ eval'' = eval.derive.derive
 evalDD :: FunExp -> Tri Func
 evalDD e = Tri (eval e, eval' e, eval'' e)
 
-{-
---Strings. Chap 4. s.s73 --
 
-foldE :: (s -> s -> s) -> (s -> s -> s) -> (Integer -> s ) -> (E -> s)
-foldE add mul con = recip
- where rec (Add x y) = add (rec x) (rec y)
-       rec (Mul x y) = mul (rec x) (rec y)
-       rec (Con i)   = con i
- 
+--Strings. Chap 4. s.s73 --
+data E = Add E E 
+       | Mul E E 
+       | Con Integer 
+ deriving Eq
  
 prTop :: E -> String 
 prTop e = let (pTop,_,_) = prVersions e
@@ -192,6 +189,167 @@ prVerCon i =
 paren :: String -> String 
 paren s = "(" ++ s ++ ")"
 
+
+foldE :: (s -> s -> s) -> (s -> s -> s) -> (Integer -> s ) -> (E -> s)
+foldE add mul con = rec
+ where rec (Add x y) = add (rec x) (rec y)
+       rec (Mul x y) = mul (rec x) (rec y)
+       rec (Con i)   = con i
+ 
+
+evalE1 :: E -> Integer
+evalE1 = foldE (+) (*) id
+
+evalE2 :: Num a => E -> a
+evalE2 = foldE (+) (*) fromInteger
+
+idE :: E -> E 
+idE = foldE Add Mul Con
+
+
+class IntExp t where
+ add :: t -> t -> t
+ mul :: t -> t -> t
+ con :: Integer -> t
+
+foldIE :: IntExp t => E -> t
+foldIE = foldE add mul con 
+
+instance IntExp E where
+  add = Add
+  mul = Mul
+  con = Con
+
+instance IntExp Integer where
+ add = (+)
+ mul = (*)
+ con = id
+
+idE' :: E -> E
+idE' = foldIE
+
+evalE' :: E -> Integer
+evalE' = foldIE
+
+-- Examples --
+seven :: IntExp a => a
+seven = add (con 3) (con 4)
+
+testI :: Integer 
+testI = seven
+
+testE :: E 
+testE = seven
+
+--End of Examples
+
+-- Chap 4. s.78 --
+
+instance Num FunExp where 
+  (+)           = (:+:)
+  (*)           = (:*:)
+  fromInteger   = Const . fromInteger
+  abs           = error "not implemented"
+  signum        = error "not implemented"
+  negate        = error "not implemeneted"
+
+instance Fractional FunExp where 
+    recip e      = Const 1 :/: e    -- returns 1 / argument
+    fromRational = Const . fromRational
+
+instance Floating FunExp where 
+  exp   = Exp
+  sin   = Sin
+  cos   = Cos
+  pi    = pi
+  log   = log 
+  asin  = asin
+  acos  = acos
+  atan  = atan
+  sinh  = sinh 
+  cosh  = cosh 
+  asinh = asinh 
+  acosh = acosh 
+  atanh = atanh
+
+
+class GoodClass t where
+  constF :: R -> t
+  addF   :: t -> t -> t
+  mulF   :: t -> t -> t
+  divF   :: t -> t -> t
+  expF   :: t -> t
+  idF    :: t
+  sinF   :: t -> t
+  cosF   :: t -> t
+
+instance Num a => GoodClass (Tri (a)) where
+ constF = evalDDConst
+ addF   = evalDDApp
+ mulF   = evalDDMul
+ divF   = evalDDDiv
+ expF   = evalDDExp
+ idF    = evalDDId
+ sinF   = evalDDSin
+ cosF   = evalDDCos
+
+
+evalDDConst = error "not implemented"
+evalDDApp   = error "not implemented"
+evalDDMul   = error "not implemented"
+evalDDDiv   = error "not implemented"
+evalDDExp   = error "not implemented"
+evalDDId    = error "not implemented"
+evalDDSin   = error "not implemented"
+evalDDCos   = error "not implemented"
+
+instance GoodClass FunExp where
+  constF = Const
+  idF  = Id
+  addF = (:+:)
+  mulF = (:*:)
+  divF = (:/:)
+  expF = Exp
+  sinF = Sin
+  cosF = Cos
+
+
+{- Kompilerar inte s.80 
+instance GoodClass (R -> R) where
+  constF = const
+  idF  = id
+  addF = (+)
+  mulF = (*)
+  ...
+  -}
+
+{- In Sec. 3.6.1, we dened a Num instance for functions with a Num codomain. If we have an
+element of the domain of such a function, we can use it to obtain a homomorphism from functions
+to their codomains: -}
+ 
+ --Num a => x -> (x -> a) -> a
+
+ {- As suggested by the type, the homomorphism is just funciton application: -}
+
+{- Kompilerar inte -
+
+apply :: a -> (a -> b) -> b
+apply a = \f -> f a
+
+om h = apply for some fixed c we have, 
+
+ h ( f + g) = {- def apply -}
+ (f + g ) c = {- def + for functions -}
+ f c + g c  = {- def apply -}
+ h f + h g etc.
+
+ -}
+
+applyTri :: a -> Tri a -> (a,a,a)
+applyTri c      (Tri (f, f', f'')) = Tri (f c, f' c, f'' c)
+
+
+ 
 --Testing--
 
 e1 = Id :*: Id -- x^2 -- deriveTripple returns correct answer.
@@ -207,4 +365,4 @@ e6 = Exp (Id :*: Id) :+: (Const 2 :*: Id) -- e^(x^2 + 2x)
 -- evalDD (x * y) = evalDD :*: evalDD y
 -- * :: FunExp -> FunExp
 -- :*:  :: Tri a -> Tri a
--}
+
